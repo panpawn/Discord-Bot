ce3b9eada70868789630f4c8511bedbb90375fdd
panpawn1 <rocketboy231@gmail.com>
2017-05-30 23:06:10 +0000

    Fix spacing in new conversions plugin

--- chat-plugins/conversions.js
+++ chat-plugins/conversions.js
@@ -9,22 +9,22 @@
 'use strict';
 
 exports.commands = {
-    temp: 'conversion',
-    c: 'conversion',
-    convert: 'conversion',
-    conversion: function (target, room, user, cmd, trigger) {
-        if (!target) return Chat.send(room, `Usage: ${trigger}${cmd} [temp][C/F] - Attempts to convert a given temperature.`);
-        let tarId = Chat.toId(target);
-        if (tarId.includes('c')) {
-            let num = tarId.substr(0, tarId.length - 1);
-            if (!Number(num)) return Chat.send(room, 'Invalid number to convert.');
-            return Chat.send(room, Math.round((num * 9/5) + 32) + '°F');
-        } else if (tarId.includes('f')) {
-            let num = tarId.substr(0, tarId.length - 1);
-            if (!Number(num)) return Chat.send(room, 'Invalid number to convert.');
-            return Chat.send(room, Math.round((num - 32) * 5/9) + '°C');
-        } else {
-            Chat.send(room, 'Unknown conversion.');
-        }
-    },
+	temp: 'conversion',
+	c: 'conversion',
+	convert: 'conversion',
+	conversion: function (target, room, user, cmd, trigger) {
+		if (!target) return Chat.send(room, `Usage: ${trigger}${cmd} [temp][C/F] - Attempts to convert a given temperature.`);
+		let tarId = Chat.toId(target);
+		if (tarId.includes('c')) {
+			let num = tarId.substr(0, tarId.length - 1);
+			if (!Number(num)) return Chat.send(room, 'Invalid number to convert.');
+			return Chat.send(room, Math.round((num * 9/5) + 32) + '°F');
+		} else if (tarId.includes('f')) {
+			let num = tarId.substr(0, tarId.length - 1);
+			if (!Number(num)) return Chat.send(room, 'Invalid number to convert.');
+			return Chat.send(room, Math.round((num - 32) * 5/9) + '°C');
+		} else {
+			Chat.send(room, 'Unknown conversion.');
+		}
+	},
 };
